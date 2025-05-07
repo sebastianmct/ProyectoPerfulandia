3e7770b9ef72f458802a4a4384755e2bee82355c
sebastianmct <seba.caamano@duocuc.cl>
2025-05-07 19:15:56 -0400

    Main

---
+++ src/main/java/duoc/cl/PerfulandiaProject/PerfulandiaProjectApplication.java
@@ -0,0 +1,13 @@
+package duoc.cl.PerfulandiaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duoc/cl/PerfulandiaProject/PerfulandiaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package duoc.cl.PerfulandiaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
